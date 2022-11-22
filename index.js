772d3f69043be90d7325eeaf53eb1c6c7a7e450f
Steve Kukla <kukla@adobe.com>
2022-11-21 23:41:59 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+console.log(`
+Steve Kukla
+
+I am a dedicated and driven professional who loves to work with others to create awesome stuff. My goals are to constantly improve, learn and grow, and to create new and innovative things that make people's lives better. I also write music for film and television.
+
+Github: https://github.com/skukla
+Music: https://stevenjkukla.com
+
+`);
